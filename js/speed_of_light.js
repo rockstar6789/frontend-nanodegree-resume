94e633c8efbbd5fb12404e16fc200b88e775daec
rockstar6789 <rockstar6789@gmail.com>
2015-12-10 16:13:29 -0500

    Add speed of light in js

---
+++ js/speed_of_light.js
@@ -0,0 +1,9 @@
+var speed_of_light_m_per_sec = 299792458;
+var one_m_in_cm = 100;
+var one_nanosecond = 1/1000000000;
+
+function speed_of_light () {
+
+ = speed_of_light_m_per_sec / one_m_in_cm / one_m_in_cm;
+
+}
