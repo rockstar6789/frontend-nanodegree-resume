a46fe9ec5b2a2319196efd40ffe32749bdb5ea8c
rockstar6789 <rockstar6789@gmail.com>
2015-12-18 16:36:40 -0500

    Save for black background if no content in index.html  - still working on this fix

---
+++ js/resumeBuilder.js
@@ -0,0 +1,192 @@
+var bio =
+    {
+      "name": "Rock Jethwa",
+      "role": "UX Architect",
+      "contacts":
+        {
+          "mobile": "1 416 834 4209",
+          "email": "rockstar6789@gmail.com",
+          "github": "rockstar6789",
+          "website": "www.RockJethwa.com"
+        },
+        "welcomeMessage": "I rate my success by how I make other people successful.",
+        "skills": [
+                    "Making useful things", "User Experience Design", "IT 3.0 Marketplaces", "Financial Service Product Innovation", "Data Visualization", "User Experience Prototyping", "Dashboards"
+                  ],
+        "bioPic": "http://cdn.slidesharecdn.com/profile-photo-rockstar6789-96x96.jpg"
+    }
+
+  var education =
+    {
+      "schools":
+      [
+        {
+          "major": "IT Management",
+          "type": "Bachelor of Commerce",
+          "name": "Ryerson University",
+          "location": "Toronto, ON, Canada"
+
+        }
+
+      ],
+      "PostGradCertifications":
+        [
+          {
+            "major": "Data Science Toolbox",
+            "type": "Certification",
+            "name": "Johns Hopkins University",
+            "url" : "http://coursera.com/"
+          },
+          {
+            "major": "Gamificaiton MBA Course",
+            "type": "Certification",
+            "name": "Wharton School of Business",
+            "url": "http://coursera.com/"
+          },
+          {
+            "major": "Front End Web Developer",
+            "type": "Nano Degree",
+            "name": "Udacity Online University",
+            "url" : "http://udacity.com/"
+          },
+          {
+            "certification": "Gamificaiton Level 3",
+            "type": "Certification",
+            "name": "Masterclass with Gabe Zickermann",
+            "url":  "http://engagementalliance.org/get-certified/"
+         }
+        ]
+    }
+
+  var work =
+  {
+    "jobs": [
+              {
+                "position" : "IT Architect",
+                "employer" : "TD Bank",
+                "years" : "Current - "
+              },
+              {
+                "position" : "Sr. UX Designer",
+                "employer" : "Questrade",
+                "years" : "2012-2014"
+              },
+              {
+                "position" : "Contract UX Designer",
+                "employer" : "RBC, TD Bank, Bell Mobility",
+                "years" : "2008-2012 "
+              },
+              {
+                "position" : "Manager Portal Services",
+                "employer" : "TELUS",
+                "years" : "2005-2008"
+              }
+            ]
+  }
+
+
+
+
+  var projects =
+    {
+    "projects":
+      [
+        {
+          "title": "IT 3.0 Marketplace",
+          "dates": "",
+          "description": "",
+          "images":
+            [
+               "", "", ""
+            ]
+        },
+        {
+          "title": "Enterprise Hackathon",
+          "dates": "",
+          "description": "",
+          "images":
+            [
+               "", "", ""
+            ]      },
+        {
+          "title": "Global Enterprise Banking UX Guide",
+          "dates": "",
+          "description": "",
+          "images":
+            [
+               "", "", ""
+            ]      },
+        {
+          "title": "Questrade Web Trading Platform",
+          "dates": "",
+          "description": "",
+          "images":
+            [
+               "", "", ""
+            ]
+          },
+        {
+          "title": "Questrade Mobile Trading App",
+          "dates": "",
+          "description": "",
+          "images":
+            [
+               "", "", ""
+            ]
+        },
+                    {
+          "title": "RBC Mobile Banking App",
+          "dates": "",
+          "description": "",
+          "images":
+            [
+               "", "", ""
+            ]
+        },
+        {
+          "title": "Bank of the Future Kiosk",
+          "dates": "",
+          "description": "",
+          "images":
+            [
+               "", "", ""
+            ]
+        },
+                    {
+          "title": "TELUS Mobile Media Player",
+          "dates": "",
+          "description": "",
+          "images":
+            [
+               "", "", ""
+            ]
+        },
+                          {
+          "title": "TELUS Mobile Search",
+          "dates": "",
+          "description": "",
+          "images":
+            [
+               "", "", ""
+            ]
+        },
+                    {
+          "title": "TELUS Mobile Portal",
+          "dates": "",
+          "description": "",
+          "images":
+            [
+               "", "", ""
+            ]
+        },
+                          {
+          "title": "TELUS Self Serve Billing Portal",
+          "dates": "",
+          "description": "",
+          "images":
+            [
+               "", "", ""
+            ]
+        }
+      ]
+    }
